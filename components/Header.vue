160b7bdfa100e470bc08da0ee31147eec0434f3c
Evgenii <voznukzhenia@gmail.com>
2020-11-24 21:27:43 +0200

    load imgs

---
+++ components/Header.vue
@@ -0,0 +1,12 @@
+<template>
+  <b-navbar toggleable="lg" type="dark" variant="dark">
+    <b-navbar-brand href="#">
+      Test App
+    </b-navbar-brand>
+  </b-navbar>
+</template>
+
+<script>
+export default {
+}
+</script>
---
+++ components/Photos.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="photos">
+    <img
+      v-for="photo in photos"
+      :key="photo.image_id"
+      class="photos__img"
+      :src="photo.src"
+      alt=""
+      @click="log"
+    >
+  </div>
+</template>
+
+<script>
+
+export default {
+  data () {
+    return {
+      photos: []
+    }
+  },
+  mounted () {
+    const api = 'https://tzfrontend.herokuapp.com/images/'
+    fetch(api)
+      .then(response => response.json())
+      .then((json) => { this.photos = json })
+  },
+  methods: {
+    log () {
+      // eslint-disable-next-line no-console
+      console.log(this.photos)
+    }
+  }
+}
+</script>
+
+<style scoped>
+.photos {
+  display: grid;
+  grid-area: auto;
+  gap: 20px;
+}
+</style>
